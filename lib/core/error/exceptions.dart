8a5b3cb10b873b1329a21566b2f3fe45bf556662
Abdulrhman Mohammed Shabaan <126196441+AbdulrhmanM0hamed@users.noreply.github.com>
2024-10-17 14:36:24 +0300

    handle error Message from server

--- lib/core/error/exceptions.dart
+++
@@ -1,9 +0,0 @@
-import 'package:movia_app/core/network/error_message_model.dart';
-
-class ServerExceptions implements Exception {
-
-  final ErrorMessageModel errorMessageModel ;
-
-  ServerExceptions({required this.errorMessageModel}); 
-  
-}
--- lib/movies/data/datasource/movie_remote_data_source.dart
+++ lib/movies/data/datasource/movie_remote_data_source.dart
@@ -1,61 +1,75 @@
+import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
-import 'package:movia_app/core/error/exceptions.dart';
+import 'package:movia_app/core/error/failuer.dart';
 import 'package:movia_app/core/network/api_constant.dart';
 import 'package:movia_app/core/network/error_message_model.dart';
 import 'package:movia_app/movies/data/models/movie_model.dart';
 
-abstract class BaseMovieRemoteDataSource{
-  Future<List<MovieModel>> getNowPlayingMovies() ;
-  Future<List<MovieModel>> getNowPopularMovies() ; 
-  Future<List<MovieModel>> getNowTopRatedMovies() ; 
- 
- 
+abstract class BaseMovieRemoteDataSource {
+  Future<Either<Failuer, List<MovieModel>>> getNowPlayingMovies();
+  Future<Either<Failuer, List<MovieModel>>> getNowPopularMovies();
+  Future<Either<Failuer, List<MovieModel>>> getNowTopRatedMovies();
 }
 
-class MovieRemoteDataSource implements BaseMovieRemoteDataSource{
-
+class MovieRemoteDataSource implements BaseMovieRemoteDataSource {
   @override
-  Future<List<MovieModel>> getNowPlayingMovies() async {
-    var response = await Dio().get(ApiConstant.nowPlayingMovie);
-
-    if (response.statusCode == 200) {
-      return List<MovieModel>.from((response.data['results'] as List)
-          .map((item) => MovieModel.fromJson(item)));
-    } else {
-      throw ServerExceptions(
-          errorMessageModel: ErrorMessageModel.fromJson(response.data));
+  Future<Either<Failuer, List<MovieModel>>> getNowPlayingMovies() async {
+    
+    try {
+      var response = await Dio().get(ApiConstant.nowPlayingMovie);
+      if (response.data['results'] != null) {
+        List<MovieModel>.from((response.data['results'] as List)
+            .map((item) => MovieModel.fromJson(item)));
+        return Right(response.data);
+      } else {
+        return Left(ServerFailure(errMessage: "No Movies found"));
+      }
+    } catch (e) {
+      if (e is DioException) {
+        return Left(ServerFailure.fromDioError(e));
+      }
+      return Left(ServerFailure(errMessage: "UnxpectedError"));
     }
   }
-  
+
   @override
-  Future<List<MovieModel>> getNowPopularMovies() async {
 
-     var response = await Dio().get(ApiConstant.popularMovie);
-
-    if (response.statusCode == 200) {
-      return List<MovieModel>.from((response.data['results'] as List)
-          .map((item) => MovieModel.fromJson(item)));
-    } else {
-      throw ServerExceptions(
-          errorMessageModel: ErrorMessageModel.fromJson(response.data));
-    }
+  Future<Either<Failuer, List<MovieModel>>> getNowPopularMovies() async {
     
+    try {
+      var response = await Dio().get(ApiConstant.popularMovie);
+      if (response.data['results'] != null) {
+        List<MovieModel>.from((response.data['results'] as List)
+            .map((item) => MovieModel.fromJson(item)));
+        return Right(response.data);
+      } else {
+        return Left(ServerFailure(errMessage: "No Movies found"));
+      }
+    } catch (e) {
+      if (e is DioException) {
+        return Left(ServerFailure.fromDioError(e));
+      }
+      return Left(ServerFailure(errMessage: "UnxpectedError"));
+    }
   }
-  
+
   @override
-  Future<List<MovieModel>> getNowTopRatedMovies() async {
+ Future<Either<Failuer, List<MovieModel>>> getNowTopRatedMovies() async {
 
-     var response = await Dio().get(ApiConstant.topRatedMovie);
-
-    if (response.statusCode == 200) {
-      return List<MovieModel>.from((response.data['results'] as List)
-          .map((item) => MovieModel.fromJson(item)));
-    } else {
-      throw ServerExceptions(
-          errorMessageModel: ErrorMessageModel.fromJson(response.data));
+    try {
+      var response = await Dio().get(ApiConstant.topRatedMovie);
+      if (response.data['results'] != null) {
+        List<MovieModel>.from((response.data['results'] as List)
+            .map((item) => MovieModel.fromJson(item)));
+        return Right(response.data);
+      } else {
+        return Left(ServerFailure(errMessage: "No Movies found"));
+      }
+    } catch (e) {
+      if (e is DioException) {
+        return Left(ServerFailure.fromDioError(e));
+      }
+      return Left(ServerFailure(errMessage: "UnxpectedError"));
     }
-
-    
-    
   }
 }
